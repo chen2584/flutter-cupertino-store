e8e3ec1d8f346418fe9aa6412b29f0a7622ad0c0
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-09-13 11:20:16 +0700

    initial commit

---
+++ lib/app.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'styles.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
+
+class CupertinoStoreHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('Cupertino Storexgsge'),
+      ),
+      child: Center(
+        child: Container(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
